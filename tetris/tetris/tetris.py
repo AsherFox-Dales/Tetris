b5a0c988c42ec0a545622dd86418b9e28363da2b
AsherFox-Dales <foxda003.309@fortismere.school>
2025-03-03 09:47:46 +0000

    .

--- tetris/tetris/tetris.py
+++ tetris/tetris/tetris.py
@@ -1,1 +1,13 @@
+import pygame as pg
 
+pg.init()
+
+console = pg.display.set_mode((1000,600))
+running = True
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+pg.draw.rect(console,(255,255,0),[100,100,100,100],0)
